50060af6a79adad9ae26693d3de2217f5a9fd2a8
Shoichi Kaji <skaji@outlook.com>
2013-12-23 23:01:55 +0900

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Dummy1 - It's new $module
+
+# SYNOPSIS
+
+    use Dummy1;
+
+# DESCRIPTION
+
+Dummy1 is ...
+
+# LICENSE
+
+Copyright (C) Shoichi Kaji.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Shoichi Kaji <skaji@outlook.com>
---
+++ lib/Dummy1.pm
@@ -0,0 +1,39 @@
+package Dummy1;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Dummy1 - It's new $module
+
+=head1 SYNOPSIS
+
+    use Dummy1;
+
+=head1 DESCRIPTION
+
+Dummy1 is ...
+
+=head1 LICENSE
+
+Copyright (C) Shoichi Kaji.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Shoichi Kaji E<lt>skaji@outlook.comE<gt>
+
+=cut
+
